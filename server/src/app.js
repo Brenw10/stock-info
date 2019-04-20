eff5da8cdda12d6b6c1b25e7cb92a6d275fc0b7f
Brendon Willian <bw.bwctm@gmail.com>
2019-04-20 14:18:07 -0300

    Added auth token for database middleware

--- server/src/app.js
+++ server/src/app.js
@@ -1,3 +1,4 @@
+const auth = require('./services/auth');
 const express = require('express');
 const app = express();
 
@@ -6,10 +7,15 @@ const users = require('./routes/users');
 app.use((_, res, next) => {
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-	// Disabling SSL certificate verify
 	process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
 	next();
 });
+
+app.use((req, _, next) =>
+	auth.getToken()
+		.then(token => req.token = token)
+		.then(() => next())
+);
 
 app.use('/users', users);
 
--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -1,11 +1,9 @@
 const express = require('express');
-const auth = require('../services/auth');
 const user = require('../services/user');
 const router = express.Router();
 
-router.get('/', (_, res) =>
-	auth.getToken()
-		.then(token => user.getUsers(token))
+router.get('/', (req, res) =>
+	user.getUsers(req.token)
 		.then(response => res.send(response))
 );
 
